b8345c2e6443c4d6aa7545849fd37ab48db3a5db
Jiahao Li <reg@ljh.me>
2023-01-13 23:13:14 -0500

    Show images

--- images/urls.py
+++ images/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:image_id>/data", views.data, name="data"),
 ]
--- images/views.py
+++ images/views.py
@@ -1,7 +1,17 @@
-from django.http import HttpResponse
+import os
+import random
+from django.http import FileResponse, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from images.models import Image
+
 
 def index(request):
-    return HttpResponse("All the pictures!!")
+    images = list(Image.objects.all())
+    random.shuffle(images)
+    return render(request, "images/image_list.html", {"images": images})
+
+
+def data(request, image_id: int):
+    image = Image.objects.get(id=image_id)
+    return FileResponse(open(os.path.join(image.repository.path, image.filename), "rb"))
