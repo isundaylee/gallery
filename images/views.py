4e4d27603d10077fcdea9b892de3d99a3ce8a5c3
Jiahao Li <reg@ljh.me>
2023-01-13 23:21:08 -0500

    Only show 30 images on index

--- images/views.py
+++ images/views.py
@@ -9,7 +9,8 @@ from images.models import Image
 def index(request):
     images = list(Image.objects.all())
     random.shuffle(images)
-    return render(request, "images/image_list.html", {"images": images})
+    return render(request, "images/image_list.html", {"images": images[:30]})
+
 
 def show(request, image_id: int):
     image = Image.objects.get(id=image_id)
